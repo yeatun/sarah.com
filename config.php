1d7befbcbcfe98cd66733fbc8fce0eca4273c4d4
Md Atiquer Rahman <35799095+AtiQ-Rahman@users.noreply.github.com>
2022-12-10 01:31:39 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
     $is_localhost=true;
     $local_folder_name = "sahara";
-    define("root", $is_localhost?"http://".$_SERVER['SERVER_NAME']."/".$local_folder_name:"https://".$_SERVER['SERVER_NAME']);
+    define("root", $is_localhost?"http://".$_SERVER['SERVER_NAME']."/".$local_folder_name:"http://".$_SERVER['SERVER_NAME']);
 ?>
