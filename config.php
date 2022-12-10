c5a07703c010e8367fd608f0e38cb970e50f5614
Md Atiquer Rahman <bsse0908@iit.du.ac.bd>
2022-12-10 18:07:55 +0600

    Solved Height Problem

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
     $is_localhost=true;
-    $local_folder_name = "sarah.com";
+    $local_folder_name = "sahara";
     define("root", $is_localhost?"http://".$_SERVER['SERVER_NAME']."/".$local_folder_name:"https://".$_SERVER['SERVER_NAME']);
 ?>
